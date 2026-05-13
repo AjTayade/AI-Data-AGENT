955014ce764760d76266571a895294a28cfd8dd1
AjTayade <ajaytayadeast@gmail.com>
2026-05-13 21:51:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
-    st.title("VibeCheck: AI Data Agent is Online")
-    st.write("If you see this, Step 1 is working!")
+st.title("VibeCheck: AI Data Agent is Online")
+st.write("If you see this, Step 1 is working!")
